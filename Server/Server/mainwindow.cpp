7754729e9da455daf5aed123b6a047d7e036c8db
a.kovalenko <darko.nerivar@gmail.com>
2017-03-13 08:18:36 +0200

    attempting to get tables prefilled

--- Server/Server/mainwindow.cpp
+++ Server/Server/mainwindow.cpp
@@ -11,9 +11,15 @@ MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
+    QString name = QString("Enter Name");
     ui->setupUi(this);
     ui->participantTable->setRowCount(1);
-//    ui->participantTable->
+    ui->participantTable->item(0,0)->setText(name);
+//    ui->participantTable->item(ui->participantTable->rowCount()-1,1)->setText("Enter Class");
+//    ui->participantTable->item(ui->participantTable->rowCount()-1,2)->setText("Enter Age");
+//    ui->participantTable->item(ui->participantTable->rowCount()-1,3)->setText("Enter Gender");
+//    ui->participantTable->item(ui->participantTable->rowCount()-1,4)->setText("Enter Rank");
+    //    ui->participantTable->
 }
 
 MainWindow::~MainWindow()
@@ -23,7 +29,12 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_addEntry_clicked()
 {
-    ui->participantTable->insertRow(1);
+    ui->participantTable->insertRow(ui->participantTable->rowCount());
+    ui->participantTable->item(ui->participantTable->rowCount()-1,0)->setText("Enter Name");
+    ui->participantTable->item(ui->participantTable->rowCount()-1,1)->setText("Enter Class");
+    ui->participantTable->item(ui->participantTable->rowCount()-1,2)->setText("Enter Age");
+    ui->participantTable->item(ui->participantTable->rowCount()-1,3)->setText("Enter Gender");
+    ui->participantTable->item(ui->participantTable->rowCount()-1,4)->setText("Enter Rank");
 }
 
 void MainWindow::on_saveTable_clicked()
@@ -33,13 +44,20 @@ void MainWindow::on_saveTable_clicked()
     QFile savefile;
     for(int i =0; i < ui->participantTable->rowCount(); i++)
     {
-        tableEntry["name"] = ui->participantTable->item(i, 0)->text();
-        tableEntry["class"] = ui->participantTable->item(i, 1)->text();
-        tableEntry["age"] = ui->participantTable->item(i, 2)->text();
-        tableEntry["gender"] = ui->participantTable->item(i, 3)->text();
-        tableEntry["rank"] = ui->participantTable->item(i, 4)->text();
+//        if (ui->participantTable->item(i, 0)->text() == QString("") ||
+//                ui->participantTable->item(i, 1)->text() == QString("")||
+//                ui->participantTable->item(i, 2)->text() == QString("")||
+//                ui->participantTable->item(i, 3)->text() == QString("")||
+//                ui->participantTable->item(i, 4)->text() == QString(""))
+//            return;
 
-        jsonTable.append(tableEntry);
+            tableEntry["name"] = ui->participantTable->item(i, 0)->text();
+            tableEntry["class"] = ui->participantTable->item(i, 1)->text();
+            tableEntry["age"] = ui->participantTable->item(i, 2)->text();
+            tableEntry["gender"] = ui->participantTable->item(i, 3)->text();
+            tableEntry["rank"] = ui->participantTable->item(i, 4)->text();
+            jsonTable.append(tableEntry);
+
     }
 
     QJsonDocument jsonDoc(jsonTable);
